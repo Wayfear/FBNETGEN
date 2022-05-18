3ed6c813920966a40fca9ffca41f90a6384e333c
XuanKan <xuan.kan@emory.edu>
2022-05-18 11:20:34 -0400

    add new dataset

--- util/abide/03-generate_abide_dataset.py
+++ util/abide/03-generate_abide_dataset.py
@@ -4,11 +4,22 @@ import os
 import numpy as np
 import argparse
 from pathlib import Path
+import pandas as pd
 
 
 def main(args):
     data_dir =  os.path.join(args.root_path, 'ABIDE_pcp/cpac/filt_noglobal/raw')
     timeseires = os.path.join(args.root_path, 'ABIDE_pcp/cpac/filt_noglobal/')
+
+    meta_file = os.path.join(args.root_path, 'ABIDE_pcp/Phenotypic_V1_0b_preprocessed1.csv')
+
+    meta_file = pd.read_csv(meta_file, header=0)
+
+    id2site = meta_file[["subject", "SITE_ID"]]
+
+    # pandas to map
+    id2site = id2site.set_index("subject")
+    id2site = id2site.to_dict()['SITE_ID']
 
     times = []
 
@@ -17,9 +28,13 @@ def main(args):
 
     corrs = []
 
+    site_list = []
+
     for f in os.listdir(data_dir):
         if osp.isfile(osp.join(data_dir, f)):
             fname = f.split('.')[0]
+            site = id2site[int(fname)]
+            
 
             files = os.listdir(osp.join(timeseires, fname))
 
@@ -45,8 +60,9 @@ def main(args):
             labels.append(label[0])
             corrs.append(att)
             pcorrs.append(pcorr)
+            site_list.append(site)
 
-    np.save(Path(args.root_path)/'ABIDE_pcp/abide.npy', {'timeseires': np.array(times), "label": np.array(labels),"corr": np.array(corrs),"pcorr": np.array(pcorrs)})
+    np.save(Path(args.root_path)/'ABIDE_pcp/abide.npy', {'timeseires': np.array(times), "label": np.array(labels),"corr": np.array(corrs),"pcorr": np.array(pcorrs), 'site': np.array(site_list)})
 
 
 if __name__ == '__main__':
